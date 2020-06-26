c06d2ebb2b7938d61e9f102f04b5ea17a58da056
Sergey Belov <sabelov@rambler.ru>
2020-06-26 15:40:37 +0300

    Initial commit

---
+++ ReadMy.md
@@ -0,0 +1,5 @@
+###### Пример Spring Boot приложения
+
+_application.properties_ - параметры
+
+http://localhost:8090/greeting?name=TestUser
---
+++ src/main/java/ru/spring_boot/WebController.java
@@ -0,0 +1,43 @@
+package ru.spring_boot;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
+
+@Controller
+public class WebController {
+    private static final Logger LOG = LogManager.getLogger(WebController.class);
+
+//    <artifactId>spring-boot-starter-thymeleaf</artifactId>
+    @GetMapping("/greeting1")
+    public String greeting1(
+            @RequestParam(
+                    name = "name",
+                    required = false,
+                    defaultValue = "World") String name,
+            Model model) {
+        model.addAttribute("name", name);
+        LOG.info("Вызов через Thymeleaf");
+        return "greetingThymeleaf";
+    }
+
+//    <artifactId>spring-boot-starter-mustache</artifactId>
+    @GetMapping("/greeting2")
+    public String greeting2(
+            @RequestParam(
+                    name = "name",
+                    required = false,
+                    defaultValue = "World") String name,
+            Map<String, Object> model) {
+        model.put("name", name);
+        LOG.info("Вызов через Mustache");
+        return "greetingMustache";
+    }
+
+}
