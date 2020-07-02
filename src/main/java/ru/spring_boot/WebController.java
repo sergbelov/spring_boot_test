be386ffb04f833d4392481972de3a1b681998348
Sergey Belov <sabelov@rambler.ru>
2020-07-02 11:27:05 +0300

    log4j -> slf4j

--- src/main/java/ru/spring_boot/WebController.java
+++ src/main/java/ru/spring_boot/WebController.java
@@ -1,8 +1,10 @@
 package ru.spring_boot;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+//import org.apache.logging.log4j.LogManager;
+//import org.apache.logging.log4j.Logger;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,14 +14,16 @@ import java.util.Map;
 
 @Controller
 public class WebController {
-    private static final Logger LOG = LogManager.getLogger(WebController.class);
+//    private static final Logger LOG = LogManager.getLogger(WebController.class);
+    private static final Logger LOG = LoggerFactory.getLogger(WebController.class);
 
     @GetMapping("/")
     public String main() {
         return "Hello world";
     }
 
-    @GetMapping("/hello")
+    //    <artifactId>spring-boot-starter-thymeleaf</artifactId>
+    @GetMapping(value = {"/hello", "/greeting1"})
     public String hello(
             @RequestParam(
                     name = "name",
@@ -31,18 +35,6 @@ public class WebController {
         return "greetingThymeleaf";
     }
 
-    //    <artifactId>spring-boot-starter-thymeleaf</artifactId>
-    @GetMapping("/greeting1")
-    public String greeting1(
-            @RequestParam(
-                    name = "name",
-                    required = false,
-                    defaultValue = "World") String name,
-            Model model) {
-        model.addAttribute("name", name);
-        LOG.info("Вызов через Thymeleaf");
-        return "greetingThymeleaf";
-    }
 
 //    <artifactId>spring-boot-starter-mustache</artifactId>
     @GetMapping("/greeting2")
